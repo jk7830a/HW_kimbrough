065c3843284eabe535d8367455111fc3b4f997f9
jk7830a <jk7830a@american.edu>
2024-09-19 13:49:00 -0400

    Print the mean and SD

--- kimbrough_hw.R
+++ kimbrough_hw.R
@@ -6,3 +6,6 @@ mean_value <- mean(random_numbers)
 
 #Compute the Standard Deviation
 sd_value <- sd(random_numbers)
+
+#Print the Mean and Standard Deviation
+print(paste("The Mean is", mean_value, "and the Standard Deviation is", sd_value))
