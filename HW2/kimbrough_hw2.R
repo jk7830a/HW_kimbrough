6c32a8fd0ce7154131553003384c31d94380d42c
jk7830a <jk7830a@american.edu>
2024-10-06 15:18:03 -0400

    HW2 Problem 2

--- HW2/kimbrough_hw2.R
+++ HW2/kimbrough_hw2.R
@@ -50,3 +50,79 @@ column_summaries
 
 #_____________________________________________________
 
+#Problem 2: 
+
+## Write a function that uses a for loop for each iteration, 
+## randomly draws 5 observations from an exponential distribution 
+## with "rate" parameter 1 (use rexp()) and calculates its "mean". 
+## It should do this 10,000 times. 
+## Choose an appropriate plot to plot the distribution of "means". 
+
+set.seed(123)
+
+draw_means_for <- function(iterations = 10000, sample_size = 5, rate = 1){
+  means <- numeric(iterations)
+  
+  for(i in 1:iterations){
+    sample <- rexp(sample_size, rate)
+    means[i] <- mean(sample)
+  }
+  return (means)
+}
+
+means_for <- draw_means_for()
+
+means_df <- data.frame(means = means_for)
+ggplot(means_df, aes(x = means)) +
+  geom_histogram(fill = "blue", color = "black") +
+  xlab("Mean") + 
+  ggtitle("Distribution of Means (For Loop)")
+
+## 2a
+### Repeat part 1 by using a map_* () function. 
+set.seed(123)
+
+means_map <- map_dbl(1:10000, ~ mean(rexp(5, rate = 1)))
+
+means_map_df <- data.frame(means = means_map)
+ggplot(means_df, aes(x = means)) +
+  geom_histogram(fill = "blue", color = "black") +
+  xlab("Mean") + 
+  ggtitle("Distribution of Means (For Loop)")
+
+## 2b
+### Repeat part 1 by using the replicate () function.
+set.seed(123)
+
+means_replicate <- replicate(10000, mean(rexp(5, rate = 1)))
+
+means_replicate_df <- data.frame(means = means_replicate)
+
+ggplot(means_replicate_df, aes(x = means)) +
+  geom_histogram(fill = "blue", color = "black") +
+  xlab("Mean") + 
+  ggtitle("Distribution of Means (replicate)")
+
+
+## 2c
+### Use another for loop that will print out plots 
+### for sample size of 5, 10, and 20 observations
+## (instead of just 5).
+sample_sizes <- c(5, 10, 20)
+
+set.seed(123)
+
+for(n in sample_sizes){
+  means <- numeric(10000)
+  
+  for(i in 1:10000){
+    sample <- rexp(n, rate = 1)
+    means[i] <- mean(sample)
+  }
+  
+  hist(means, main = paste("Distribution of Means (Sample Size:", n, ")"),
+       xlab = "Mean", breaks = 30)
+}
+
+#_____________________________________________________
+
